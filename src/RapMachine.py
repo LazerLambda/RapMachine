b5b3580f199d97b385fb604805316bb1160b9fba
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2022-01-20 03:19:40 +0100

    Add CreateAdvData script to create training data for ranking task

--- src/RapMachine.py
+++ src/RapMachine.py
@@ -11,27 +11,44 @@ Authors:
 2022
 """
 
+import transformers
+
 
 class RapMachine:
     """Rap Machine Class."""
 
     model: any = None
 
-    def __init__(self):
+    def __init__(
+            self,
+            lm_str: str,
+            model_path: str,
+            gen_config: dict = {
+                "num_beams": 5,
+                'min_length': 400,
+                'top-k': 50}):
         """Initialize Class."""
+        self.lm_str: str = lm_str
+        self.model_path: str = model_path
+        self.gen_config: dict = gen_config
+
+        self.generator: transformers.Pipeline = None
         pass
 
     def load_model(self, **args) -> None:
         """Load Model."""
-        pass
-
-    def fine_tune(self, **args) -> None:
-        """Fine-tune Model."""
-        pass
+        self.generator = transformers.pipeline('text-generation',
+            model=self.model_path,
+            tokenizer=self.lm_str,
+            config = self.gen_config
+        )
 
     def generate(self, input: list, amount: int) -> list:
         """Generate Samples."""
-        return []
+        candidates: list = []
+        for i in range(amount):
+            candidates.append(self.generator(input)[0]['generated_text'])
+        return candidates
 
     def rank(self, candidates: list) -> list:
         """Rank Outputs."""
