b7bff134e3f4b023324dc350c56f653c00facca9
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2022-01-31 14:52:58 +0100

    Update install.sh, add ranker

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ echo "CONSUMER_API_KEY=\nCONSUMER_API_KEY_SECRET=\nBEARER_TOKEN=\nACCESS_TOKEN=\
 echo "Empty .env file created. Please provide necessary credentials: https://developer.twitter.com/en"
 
 mkdir .model
+cd .model
+mkdir GPT2-rap-recommended
+cd ..
 echo "Empty .model folder created. Please download and extract the model from https://drive.google.com/drive/folders/116WlytHENvyNia_xZr7GxUEym20SjeQn?usp=sharing"
 
 FILE=lid.176.bin
